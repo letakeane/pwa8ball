5b939b4ba7cc9cf1d8b94e5dc5d4a5f08fe0e0fb
Leta Keane <letakeane+turing@gmail.com>
2019-01-31 11:33:40 -0700

    Fix server port error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.NODE_ENV || 'development');
+app.set('port', process.env.NODE_ENV || 3000);
 app.use(express.static('public'));
 
 app.listen(app.get('port'), () => console.log(`You are now running PW8ball on localhost:${app.get('port')}!`));
